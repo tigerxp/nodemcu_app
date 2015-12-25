958b88f2c443cc38a213410574765925bdf3b4e0
tiger <s.garmash@softerra.com>
2015-12-25 09:01:34 +0200

    Added: WiFi reconnect if connection lost

--- client.lua
+++ client.lua
@@ -5,6 +5,8 @@ http = require('http_client')
 WIFI_MAX_RETRY = 100
 TEMP_DISABLE = 55
 TEMP_ENABLE = 60
+LOOP_TIMER = 2
+WIFI_TIMER = 0
 
 PUMP_ON = gpio.LOW
 PUMP_OFF = gpio.HIGH
@@ -12,6 +14,7 @@ PUMP_OFF = gpio.HIGH
 data = {}
 ip_addr = nil
 wifi_attempt = 1 -- Counter of trys to connect to wifi
+wifi_connected = false
 iteration = 0
 
 sensors = {
@@ -28,10 +31,8 @@ end
 function loop()
     data['field1'] = node.heap()
     data['field2'] = tmr.time() -- uptime
-    -- TODO: replace with real reading
-    -- sensors.temp = helpers.round(math.random(40*100, 70*100)/100, 2)
+    -- Read DS1820
     sensors.temp = sensors.ds.read();
-    print('DS temperature: ', sensors.temp)
     if (sensors.temp < TEMP_DISABLE) then
         sensors.pump = PUMP_OFF
     end
@@ -44,44 +45,42 @@ function loop()
     print("Data: ", data['field1'], data['field2'], data['field3'], data['field4'])
 
     -- At this point, data table should be ready to be sent
-    http.get(config.ts_address, config.ts_port, config.ts_url, data, function(status, data, message)
-        if (data ~= '') then
-            print(iteration, status, message)
-        else
-            print("Empty response!")
-        end
-    end)
+    if (wifi_connected) then
+        http.get(config.ts_address, config.ts_port, config.ts_url, data, function(status, data, message)
+            if (data ~= '') then
+                print(iteration, status, message)
+            else
+                print("Empty response!")
+            end
+        end)
+    end
     iteration = iteration+1
 --    collectgarbage("collect")
 end
 
 -- Change the code of this function that it calls your code.
-function launch()
-    print('Connected to WIFI!')
+function wifiConnected()
+    print('WiFi connected.')
     print('IP Address: ' .. wifi.sta.getip())
-    -- Call setup
-    setup()
-    -- Set globals
-    data['api_key'] = config.ts_api_key
-    -- Call the prog in a loop
-    print('Start sending data, interval (s):' .. config.loop_time)
-    -- First run
-    loop()
-    -- Loop run
-    tmr.alarm(0, config.loop_time * 1000, 1, loop)
+    wifi_connected = true
+    -- Re-arm WiFi check
+    wifi_attempt = 1 -- reset counter
+    tmr.alarm(WIFI_TIMER, 60000, 0, checkWIFI) -- Call checkWIFI in 1min
 end
 
 function checkWIFI()
     if (wifi_attempt > WIFI_MAX_RETRY) then
-        print('Unable to connect after ' .. WIFI_MAX_RETRY .. ' tries.' )
+        print('Unable to connect after ' .. WIFI_MAX_RETRY .. ' tries. Reboot.' )
+        node.restart()
     else
         ip_addr = wifi.sta.getip()
         if ((ip_addr ~= nil) and (ip_addr ~= '0.0.0.0')) then
-            -- lauch()        -- Cannot call directly the function from the timer... NodeMcu crashes...
-            tmr.alarm(1, 500, 0, launch)
+            -- Cannot call directly the function from the timer... NodeMcu crashes...
+            tmr.alarm(1, 500, 0, wifiConnected)
         else
+            wifi_connected = false
             -- Reset alarm again
-            tmr.alarm(0, 3000, 0, checkWIFI)
+            tmr.alarm(WIFI_TIMER, 3000, 0, checkWIFI) -- check every 3s
             print('Checking Wifi, attempt ' .. wifi_attempt .. '...')
             wifi_attempt = wifi_attempt + 1
         end
@@ -97,8 +96,17 @@ if ((ip_addr == nil) or (ip_addr == '0.0.0.0')) then
     wifi.setmode(wifi.STATION)
     wifi.sta.config(config.wifi_ssid, config.wifi_password)
     print('Waiting for WiFi connection...')
-    tmr.alarm(0, 2500, 0, checkWIFI) -- Call checkWIFI 2.5S in the future.
+    tmr.alarm(WIFI_TIMER, 2500, 0, checkWIFI) -- Call checkWIFI 2.5S in the future.
 else
-    -- Connected, launch the code
-    launch()
+    -- Already connected
+    wifiConnected()
 end
+
+-- Set globals
+data['api_key'] = config.ts_api_key
+setup()
+-- Call the prog in a loop
+print('Start processing sensors, interval (s):' .. config.loop_time)
+loop() -- First run
+-- Loop run
+tmr.alarm(LOOP_TIMER, config.loop_time * 1000, 1, loop)
